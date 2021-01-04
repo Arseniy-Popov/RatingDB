47b263f0d20917b72c9cd82c3086e70cc9206c97
Arseniy Popov <arseniypopov@gmail.com>
2021-01-04 18:02:09 +0000

    -

--- docs/renderdocs.sh
+++ docs/renderdocs.sh
@@ -1,2 +1,2 @@
-npx redoc-cli bundle -o ./redoc.html spec.yaml
-python swagger-yaml-to-html.py < ./spec.yaml > ./swagger.html
+npx redoc-cli bundle -o ./static/redoc.html spec.yaml
+python swagger-yaml-to-html.py < ./spec.yaml > ./static/swagger.html
